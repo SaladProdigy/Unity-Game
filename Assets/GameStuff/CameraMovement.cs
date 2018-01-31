3dafbc571a23a9c1311518080e4b92b8cc1afd72
Unknown <rr3149@nyu.edu>
2018-01-31 13:50:22 -0500

    Project

---
+++ Assets/GameStuff/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public GameObject objectToFollow;
+    Vector3 cameraOffSet;
+
+
+    // Use this for initialization
+    void Start () {
+        cameraOffSet = new Vector3(1, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = this.transform.position;
+        position.y = Mathf.Lerp(this.transform.position.y, objectToFollow.transform.position.y, 50f);
+        position.x = Mathf.Lerp(this.transform.position.x, objectToFollow.transform.position.x, 50f);
+
+        this.transform.position = position + cameraOffSet;
+    }
+
+}
+
---
+++ Assets/Player/PlayerScripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed; //Player Speed
+    public float rb;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Escape key to quit the game
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        float hormovement = Input.GetAxis("Horizontal") * speed; //Allows left + right movement
+
+        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(hormovement, 0), ForceMode2D.Impulse); 
+
+    }
+}
